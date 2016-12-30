e512aeac04a49c1762b1acda9dbc65ea310769bc
Hendra Widjaja <hendrawidjaja@live.de>
2016-12-30 23:31:45 +0100

    Http

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function(req, resp, next) {
-    if (req.header['x-forwarded-proto'] === 'http') {
-        next();
-    } else {
-        resp.redirect('http://' + req.hostname + req.url);
-    }
-});
+// app.use(function(req, resp, next) {
+//     if (req.header['x-forwarded-proto'] === 'http') {
+//         next();
+//     } else {
+//         resp.redirect('http://' + req.hostname + req.url);
+//     }
+// });
 
 app.use(express.static('public'));
 
