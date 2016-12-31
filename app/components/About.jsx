85a6ce8e065f4fb150e62f4512b1e9bfbf299b73
Hendra Widjaja <hendrawidjaja@live.de>
2016-12-31 14:58:33 +0100

    Touch About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,9 +3,18 @@ var React = require('react');
 var About = (props) => {
     return (
         <div>
-            <h3>About</h3>
-            <p>Welcome to About page!</p>
-        </div>    
+            <h1 className="text-center">About</h1>
+            <p>Small react project. I have built this for developer course.</p>
+            <p>Here are some of the tools you need to have a loook:</p>
+            <ul>
+                <li>
+                    <a href="htpps://www.facebook.github.io/react">The React Framework</a>
+                </li>
+                <li>
+                    <a href="htpps://www.openweathermap.org">Open Weather Map</a>
+                </li>
+            </ul>
+        </div>
     )
 };
 
